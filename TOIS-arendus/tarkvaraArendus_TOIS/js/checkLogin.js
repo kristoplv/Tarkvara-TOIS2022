80ab168d5aa13ae432dd5a208fb9df27a9713cb4
Kristo Põlluvee <krisplv@tlu.ee>
2022-06-08 15:10:27 +0300

    more changes to login

--- TOIS-arendus/tarkvaraArendus_TOIS/js/checkLogin.js
+++ TOIS-arendus/tarkvaraArendus_TOIS/js/checkLogin.js
@@ -1,5 +1,7 @@
 var email = document.querySelector("#email");
 var pw = document.querySelector("#password");
+var pwLogo = document.querySelector(".password_logo");
+var emailLogo = document.querySelector(".human_logo");
 var noticeBox = document.getElementById("notice");
 
 var login = document.querySelector("#loginButton");
@@ -8,15 +10,27 @@ function checkForValues(){
     var notice = "";
     if(email.value == ""){
         notice += "Meiliaadress lisamata! <br>"
+        email.style.border = "2px solid red";
         noticeBox.style.color = "red";
+        emailLogo.style.border = "2px solid red";
+        email.style.borderLeft = "";
+        emailLogo.style.borderRight = "";
     }
     if(pw.value == ""){
         notice += "Parool lisamata!";
         noticeBox.style.color = "red";
+        pw.style.border = "2px solid red";
+        pwLogo.style.border = "2px solid red";
+        pw.style.borderLeft = "";
+        pwLogo.style.borderRight = "";
     }
     if(notice == ""){
         notice = "Vajalikud väljad täidetud!";
         noticeBox.style.color = "green";
+        pw.style.border = "";
+        email.style.border = "";
+        emailLogo.style.border = "";
+        pwLogo.style.border = "";
     }
     console.log(notice);
     noticeBox.innerHTML = "<p1 class='login'>" + notice + "</p1>";
@@ -24,5 +38,15 @@ function checkForValues(){
 
 }
 
+function revertHightlightEmail(){
+    email.style.border = "";
+    emailLogo.style.border = "";
+}
+function revertHightlightPw(){
+    pw.style.border = "";
+    pwLogo.style.border = "";
+}
 
 login.addEventListener("click", checkForValues);
+email.addEventListener("click", revertHightlightEmail);
+pw.addEventListener("click", revertHightlightPw);
