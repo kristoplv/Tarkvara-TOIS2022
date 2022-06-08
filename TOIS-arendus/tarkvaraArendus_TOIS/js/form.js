905a17d6931173d28d1140100cc8cdd2dbaecb12
robkukke <90237340+robkukke@users.noreply.github.com>
2022-06-08 11:58:08 +0300

    Tabeli põhi osaliselt valmis

---
+++ TOIS-arendus/tarkvaraArendus_TOIS/js/form.js
@@ -0,0 +1,43 @@
+function openTable1() {
+    let table = document.getElementById('table1');
+    let hide = table.style.display == 'none';
+
+    if (hide) {
+        table.style.display = 'table';
+    } else {
+        table.style.display = 'none';
+    }
+}
+
+function openTable2() {
+    let table = document.getElementById('table2');
+    let hide = table.style.display == 'none';
+
+    if (hide) {
+        table.style.display = 'table';
+    } else {
+        table.style.display = 'none';
+    }
+}
+
+function openTable3() {
+    let table = document.getElementById('table3');
+    let hide = table.style.display == 'none';
+
+    if (hide) {
+        table.style.display = 'table';
+    } else {
+        table.style.display = 'none';
+    }
+}
+
+function openTable4() {
+    let table = document.getElementById('table4');
+    let hide = table.style.display == 'none';
+
+    if (hide) {
+        table.style.display = 'table';
+    } else {
+        table.style.display = 'none';
+    }
+}
