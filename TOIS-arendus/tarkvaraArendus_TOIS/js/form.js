5eeb9561d6bdb92d7dfd1c15f8e16924e7648a50
robkukke <90237340+robkukke@users.noreply.github.com>
2022-06-09 11:20:28 +0300

    Simplified openTable function

--- TOIS-arendus/tarkvaraArendus_TOIS/js/form.js
+++ TOIS-arendus/tarkvaraArendus_TOIS/js/form.js
@@ -1,38 +1,6 @@
-function openTable1() {
-    let table = document.getElementById('table1');
-    let hide = table.style.display == 'none';
-
-    if (hide) {
-        table.style.display = 'table';
-    } else {
-        table.style.display = 'none';
-    }
-}
-
-function openTable2() {
-    let table = document.getElementById('table2');
-    let hide = table.style.display == 'none';
-
-    if (hide) {
-        table.style.display = 'table';
-    } else {
-        table.style.display = 'none';
-    }
-}
-
-function openTable3() {
-    let table = document.getElementById('table3');
-    let hide = table.style.display == 'none';
-
-    if (hide) {
-        table.style.display = 'table';
-    } else {
-        table.style.display = 'none';
-    }
-}
-
-function openTable4() {
-    let table = document.getElementById('table4');
+function openTable(tableBtn_id) {
+    let table_id = tableBtn_id.replace('Btn', '');
+    let table = document.getElementById(table_id);
     let hide = table.style.display == 'none';
 
     if (hide) {
