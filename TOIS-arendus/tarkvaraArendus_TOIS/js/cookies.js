60c501fbe7949c99210f9471448a5d4cad16ccf9
Christian-Enrique Hindremäe <chris55@tlu.ee>
2022-06-13 15:05:44 +0300

    sisselogimise küpsis töötab

--- TOIS-arendus/tarkvaraArendus_TOIS/js/cookies.js
+++ TOIS-arendus/tarkvaraArendus_TOIS/js/cookies.js
@@ -1,11 +1,20 @@
 let email = document.querySelector('#email');
+/*let language = document.querySelector('#')*/
 
 window.onload = function(){
     document.querySelector('#loginButton').addEventListener('click', storeData);
+
+    loadSavedLogin();
 }
 
 function storeData(){
     window.localStorage.setItem('login', JSON.stringify(email.value));
-    loginCookie = JSON.parse(window.localStorage.getItem('login'));
+    let loginCookie = JSON.parse(window.localStorage.getItem('login'));
     console.log(loginCookie);
+}
+
+function loadSavedLogin(){
+    if(JSON.parse(window.localStorage.getItem('login')) !== null){
+        email.value = JSON.parse(window.localStorage.getItem('login'));
+    }
 }
