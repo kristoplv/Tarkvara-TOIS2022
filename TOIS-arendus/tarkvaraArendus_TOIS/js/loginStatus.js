d2e941080979606c25b62ab20b96eaca1c97a900
Christian-Enrique Hindremäe <chris55@tlu.ee>
2022-06-15 14:30:24 +0300

    defer

--- TOIS-arendus/tarkvaraArendus_TOIS/js/loginStatus.js
+++ TOIS-arendus/tarkvaraArendus_TOIS/js/loginStatus.js
@@ -2,6 +2,7 @@ let loginButton = document.querySelector('#logIn');
 
 window.onload = function() {
     checkLoginStatus();
+    console.log(JSON.parse(window.localStorage.getItem('session')));
 }
 
 function checkLoginStatus(){
