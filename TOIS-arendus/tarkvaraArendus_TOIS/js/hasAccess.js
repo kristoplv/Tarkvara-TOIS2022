a2b26507a503ee2a9cce8a0b0a2187c1710b35e0
Christian-Enrique Hindremäe <chris55@tlu.ee>
2022-06-15 15:47:24 +0300

    currentPage

--- TOIS-arendus/tarkvaraArendus_TOIS/js/hasAccess.js
+++ TOIS-arendus/tarkvaraArendus_TOIS/js/hasAccess.js
@@ -2,6 +2,6 @@ let session = JSON.parse(window.localStorage.getItem('session'));
 
 function authAccess(){
     if(session == null){
-        window.location="http://localhost:3001/login.html";
+        window.location="login.html";
     }
 }
---
+++ TOIS-arendus/tarkvaraArendus_TOIS/js/navigation.js
@@ -0,0 +1,5 @@
+let address = window.location.href;
+
+if(address.includes("main")){
+    document.querySelector('#homePage').classList.add('currentPage');
+}
