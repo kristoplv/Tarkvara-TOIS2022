2bf5ac46b4788b9c95d48d2c4a10722e30cff819
Germo Mäenurm <90237415+germomaenurm@users.noreply.github.com>
2022-06-17 11:23:55 +0300

    search update

--- TOIS-arendus/tarkvaraArendus_TOIS/js/search.js
+++ TOIS-arendus/tarkvaraArendus_TOIS/js/search.js
@@ -1,6 +1,6 @@
 const searchInput = document.getElementById("searchinput");
 
-const namesFromDOM = document.getElementsByClassName("topper");
+const namesFromDOM = document.getElementsByClassName("kavad");
 
 searchInput.addEventListener("keyup", (event) => {
     const { value } = event.target;
@@ -9,6 +9,7 @@ searchInput.addEventListener("keyup", (event) => {
     
     for (const nameElement of namesFromDOM) {
         let name = nameElement.textContent.toLowerCase();
+        
         
         if (name.includes(searchQuery)) {
             nameElement.style.display = "";
